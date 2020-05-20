2e82418bc9e0404f51352c26b89a555096fcc8bd
yuivy <sy2021x@gmail.com>
2020-05-21 00:18:10 +0900

    定数クラスの作成

---
+++ app/services/constract_group.rb
@@ -0,0 +1,17 @@
+class ConstractGroup
+  def self.tomorrow
+    ["明日", "あした"]
+  end
+
+  def self.day_after_tomorrow
+    ["明後日", "あさって"]
+  end
+
+  def self.praise
+    ["かわいい", "ありがとう", "すき", "好き", "おもしろい", "すごい", "すご", "がんばれ", "ガンバ"]
+  end
+
+  def self.greeting
+    ["こんにちは","こんばんは", "おはよう"]
+  end
+end
